b4cb01444bd3de476188713ef456b1b2ca5f1763
Ivan Botero <ivan.botero@protonmail.ch>
2020-07-14 13:36:02 -0500

    Subidas dos consultas que faltaban

--- SCSIPS/ConsultaDos.sql
+++ SCSIPS/ConsultaDos.sql
@@ -1,15 +1,25 @@
 SELECT
     u.nombre,
     u.apellido,
-    tc.valor,
+    td.valor,
     u.documento,
-    COUNT(i.identificador)
+    COUNT(i.identificador) AS cantidad
 FROM
-    usuario          u
-    JOIN tipo_documento   tc ON tc.identificador = u.identificador_tipo_documento
-    JOIN incidente        i ON i.identificador_usuario = u.identificador
+    usuario            u
+    JOIN tipo_documento     td ON td.identificador = u.identificador_tipo_documento
+    JOIN incidente          i ON i.identificador_usuario = u.identificador
+    JOIN estado_incidente   ei ON ei.identificador = i.identificador_estado_incidente
+WHERE
+    ei.valor IN (
+        'Pending',
+        'Solving'
+    )
 GROUP BY
     u.nombre,
     u.apellido,
-    tc.valor,
+    td.valor,
     u.documento
+HAVING
+    COUNT(i.identificador) > 0
+ORDER BY
+    cantidad DESC;
--- SCSIPS/ConsultaTres.sql
+++ SCSIPS/ConsultaTres.sql
@@ -1,11 +1,25 @@
-SELECT
-    i.identificador,
-    ti.valor,
-    hi.identificador_estado_incidente,
-    ei.valor,
-    hi.fecha
+SELECT DISTINCT
+    ei.valor AS estado,
+    COUNT(ei.identificador) AS cantidad
 FROM
     incidente             i
     JOIN tipo_incidente        ti ON ti.identificador = i.identificador_tipo_incidente
-    JOIN historico_incidente   hi ON hi.identificador_incidente = i.identificador
     JOIN estado_incidente      ei ON ei.identificador = i.identificador_estado_incidente
+    JOIN historico_incidente   hi ON hi.identificador_incidente = i.identificador
+WHERE
+    hi.fecha BETWEEN TO_DATE('01-01-20', 'DD-MM-YY') AND TO_DATE('31-12-20', 'DD-MM-YY')
+    AND ti.valor IN (
+        'Bug',
+        'Feature'
+    )
+GROUP BY
+    ei.valor
+ORDER BY
+    CASE estado
+        WHEN 'Pending'   THEN
+            '0'
+        WHEN 'Solving'   THEN
+            '1'
+        WHEN 'Solved'    THEN
+            '2'
+    END;
